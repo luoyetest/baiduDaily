f7b9c332ef17e79b43e7e50f5b806c80f5921c29
zhouxingzhi <zhouxingzhi@live.com>
2016-07-28 15:53:05 +0800

    修改账户信息

--- config/config.js
+++ config/config.js
@@ -2,10 +2,10 @@
  * Created by Administrator on 2016/7/27.
  */
 module.exports.luoye = {
-    cookie: 'BAIDUID=AB41711091ECC71D5DCCF0D7F133ED37:FG=1; BDUSS=UtVFVxOG1GajVOYndHTW5jYWU0Ym4xOElRNlRsbDRWTFZBRW4tfm0tUUxEc0JYQVFBQUFBJCQAAAAAAAAAAAEAAADF-AkhyvHs4cLk0rkAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAuBmFcLgZhXWm;',
+    cookie: 'BAIDUID=***********; BDUSS=************;',
     post_data : {
         ie: 'ie:utf-8',
-        tbs: '5ce2e73c1c7daa951469612727'
+        tbs: '*********'
     },
     email: '405975383@qq.com'
 };
